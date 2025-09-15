396160999b209178df8fe0667fbaba2dd52f1d09
Joseph Youskievicz <jyouskievicz@my365.bellevue.edu>
2025-09-15 00:23:20 -0500

    Completed Assignment 1.2 HelloWorldApp

---
+++ Module_1/HelloWorldApp/YouskieviczHelloWorld.java
@@ -0,0 +1,13 @@
+package Module_1.HelloWorldApp;
+
+public class YouskieviczHelloWorld {
+    public static void main(String[] args) {
+        // Store your first and last name in variables
+        String firstName = "Joseph";
+        String lastName = "Youskievicz";
+
+        // Print the result using printf
+        System.out.printf("Hello World from %s %s%n", firstName, lastName);
+    }
+}
+
