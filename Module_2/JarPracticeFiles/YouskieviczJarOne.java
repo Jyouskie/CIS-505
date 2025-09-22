29f89df3133a8d86f8888ced1d2a165b9edbb374
Joseph Youskievicz <jyouskievicz@my365.bellevue.edu>
2025-09-22 15:02:12 -0500

    creating files and folders for jar assignment

---
+++ Module_2/JarPracticeFiles/YouskieviczJarOne.java
@@ -0,0 +1,8 @@
+package Module_2.JarPracticeFiles;
+
+public class YouskieviczJarOne {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jar One!");
+    }
+    
+}
---
+++ Module_2/JarPracticeFiles/YouskieviczJarTwo.java
@@ -0,0 +1,7 @@
+package Module_2.JarPracticeFiles;
+
+public class YouskieviczJarTwo {
+     public static void main(String[] args) {
+        System.out.println("Hello from Jar Two!");
+     }
+}
